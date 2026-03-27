167ee84bbd2ac0d4ea769551188e39b63269ca9e
pedro <pedroalbertoramos31@gmail.com>
2026-03-26 19:44:14 -0700

    conexion sql

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const pool = require('./db_connection');
+
 
 app.get('/', (req, res) => {
   res.send('API funcionando');
@@ -15,6 +17,17 @@ app.get('/usuario', (req, res) => {
   res.json(usuario);
 });
 
+pool.connect()
+  .then(() => {
+    console.log('Conexión exitosa a PostgreSQL');
+  })
+  .catch((err) => {
+    console.error('Error de conexión', err);
+  });
+
+
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
+
+
